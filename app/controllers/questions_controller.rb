5cfa6bec40512187c4b4d1bb36c0de6f48fab926
OldsterJoey <jjcsull@gmail.com>
2021-08-01 19:59:15 +1000

    Added NOTNULL constraint to questions table, currently still allowing null however. Also added some tests to client.http for question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,18 @@ class QuestionsController < ApplicationController
         @questions = Question.all
         render json: @questions
     end
+
+    def create
+        @question = Question.create(question_params)
+        if @question.errors.any?
+            render json: @question.errors, status: :unprocessable_entity 
+        else
+            render json: @question, status: 201
+        end
+    end
+
+    private
+    def question_params
+        params.permit(:question, :answer)
+    end
 end
--- db/migrate/20210801054540_create_questions.rb
+++ db/migrate/20210801054540_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-      t.text :question
-      t.string :answer
+      t.text :question, null: false
+      t.string :answer, null: false
 
       t.timestamps
     end
