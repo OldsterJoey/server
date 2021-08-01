3b1161b6bf42a266ff9017875940fd09f30f53ba
OldsterJoey <jjcsull@gmail.com>
2021-08-01 14:50:44 +1000

    Created adminquestion routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    get '/adminquestions', to: 'adminquestions#index'
+    post '/adminquestions', to: 'adminquestions#create'
+    get '/adminquestions/:id', to: 'adminquesions#show'
+    put '/adminquestions/:id', to: 'adminquesions#update'
+    delete '/adminquestions/:id', to: 'adminquesions#destroy'
+    get '/adminquestions/random', to: 'adminquestions#random'
+  end
 end
+ 
