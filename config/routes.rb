0c8dca08cfaccaeb7bacf46a38c7b8b89d04c50d
OldsterJoey <jjcsull@gmail.com>
2021-08-07 11:35:24 +1000

    Created initial user post route and create a client.http request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/api' do
+    # User Routes
+    scope '/auth' do
+      post '/sign_up', to: 'users#create'
+      
+    end
     # Admin Profile Routes
     get '/admin_profiles', to: 'admin_profiles#index'
     post '/admin_profiles', to: 'admin_profiles#create'
