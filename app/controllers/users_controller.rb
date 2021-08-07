931f991774bd1303775364b236954a53695848bf
OldsterJoey <jjcsull@gmail.com>
2021-08-07 11:40:20 +1000

    added initial jwt token to user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.save
-            render json: @user, status: :created
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {username: @user.username, jwt: auth_token.token}, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
         end
