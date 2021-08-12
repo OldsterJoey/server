db45e3cfabdcd3c3bfa10ec864ce9192468e9543
OldsterJoey <jjcsull@gmail.com>
2021-08-12 18:17:53 +1000

    Added deployed client to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:5050', 'https://wishfully-server.herokuapp.com'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:5050', 'https://wishfully-server.herokuapp.com', 'https://wishfully-2021.web.app'
 
     resource '*',
       headers: :any,
