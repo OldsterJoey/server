54154caf7a38a3faf408ebc9ef564b5ab0f656ae
OldsterJoey <jjcsull@gmail.com>
2021-08-12 16:20:18 +1000

    fixed syntax errors in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:3000', 'https://localhost:3001', 'https://127.0.0.1:5050', 'https://wishfully-server.herokuapp.com'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:5050', 'https://wishfully-server.herokuapp.com'
 
     resource '*',
       headers: :any,
