205800c07bad55aedf277b4d4479327f585c311b
OldsterJoey <jjcsull@gmail.com>
2021-08-15 14:43:44 +1000

    Started creation of user factory and child profiles post testing

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
     factory :user do
         User.create(username: "Joey", email: "joey@test.com", password: "joey12321", password_confirmation: "joey12321")
+        User.create(username: "Irina", email: "irina@test.com", password: "irina12321", password_confirmation: "irina12321")
     end
 end
--- spec/requests/child_profiles_spec.rb
+++ spec/requests/child_profiles_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 describe 'child_profiles', type: :request do
-    it 'returns at least two child_profiles' do
-        FactoryBot.create(:child_profile, user_id: 1 ,name: 'Little Timmy',)
-        get '/api/child_profiles'
+    describe '/GET child_profiles' do
+        it 'returns two child_profiles' do
+            FactoryBot.create(:child_profile, user_id: 1 ,name: 'Little Timmy',)
+            FactoryBot.create(:child_profile, user_id: 1 ,name: 'Little Jimmy',)
+            get '/api/child_profiles'
+    
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(2)
+        end       
+    end
 
-        expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).size).to eq(2)
+    describe '/POST child_profiles' do
+        it 'creates a new child_profile' do
+            expect {
+                post '/api/child_profiles', params: { child_profile: { id: 1, name: 'Little Bimmy' } }
+        }.to change { ChildProfile.count }.from(0).to(1)
+
+            expect(response).to have_http_status(201)
+        end
     end
 end
 
